6e3f709343562e54151152628af8d312afc732ab
Farshad <farshad.basereh@gmail.com>
2020-05-13 16:26:39 -0400

    extract function for better reuse in other components

--- src/components/ResourceList/ResourceList.js
+++ src/components/ResourceList/ResourceList.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { getResource } from '../../services/appApis';
 
-const ResourceList = ({ resource }) => {
+//Extract hooks related logic into a separate function
+const useResources = (resource) => {
   const [resources, setResources] = useState([]);
 
   const fetchResource = (resource) => {
     getResource(resource)
     .then(response => {
-      console.log(response);
       setResources(response);
     })
   };
@@ -16,6 +16,13 @@ const ResourceList = ({ resource }) => {
     fetchResource(resource);
   }, [resource]);
 
+  return resources;
+}
+
+const ResourceList = ({ resource }) => {
+  //call the useResources function to render logic into ResourceList comp. 
+  const resources = useResources(resource);
+  
     return (
       <div>
         {resources.map(record =>{
